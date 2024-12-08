178a1d670a2936637d8b45a9023b27a4d49c91c5
shindesc <sarashinde2061@gmail.com>
2024-12-08 13:49:23 -0500

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -7,9 +7,18 @@ data Found = Match Int | NoMatch deriving Eq
 instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
+
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
+findFirst predicate xs = go 0 xs
+  where
+    go _ [] = NoMatch
+    go idx (y:ys)
+      | predicate y = Match idx
+      | otherwise = go (idx + 1) ys
 
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
+palindrome xs = xs == reverse xs
+
